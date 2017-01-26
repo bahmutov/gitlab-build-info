3e1a71e5148e27e7fd67c69eb5d9d8a276f8801c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-01-26 17:40:56 -0500

    feat(code): initial tested code

--- src/gitlab-build-info-spec.js
+++ src/gitlab-build-info-spec.js
@@ -1,7 +1,33 @@
 'use strict'
 
 /* global describe, it */
-describe('empty test suite', () => {
-  it('write this test', () => {
+const pkg = require('../package.json')
+const la = require('lazy-ass')
+const is = require('check-more-types')
+
+describe(pkg.name, () => {
+  const grabGitLabInfo = require('.')
+  const mockEnv = {
+    CI: 1,
+    CI_PROJECT_URL: 'https://server.com/user/test',
+    CI_BUILD_NAME: 'test-spec',
+    CI_BUILD_ID: 101,
+    CI_PIPELINE_ID: 550
+  }
+
+  it('is a function', () => {
+    la(is.fn(grabGitLabInfo))
+  })
+
+  it('returns function that collects info from the mock env', () => {
+    const {collectionInformation} = grabGitLabInfo(mockEnv)
+    la(is.fn(collectionInformation))
+  })
+
+  it('can collect info', () => {
+    const {collectionInformation} = grabGitLabInfo(mockEnv)
+    const info = collectionInformation(pkg.name)
+    la(is.object(info), 'did not get info object', info)
+    la(info.specName === mockEnv.CI_BUILD_NAME, 'wrong spec name', info)
   })
 })
--- src/index.js
+++ src/index.js
@@ -1,1 +1,65 @@
 'use strict'
+
+const la = require('lazy-ass')
+const is = require('check-more-types')
+
+function init (env) {
+  env = env || process.env
+
+  const isOnGitLab = Boolean(env.CI)
+
+  function collectionInformation (projectName) {
+    la(is.maybe.string(projectName), 'expected project name', projectName)
+    la(isOnGitLab, 'cannot get information if not running on GitLab')
+
+    la(is.url(env.CI_PROJECT_URL), 'invalid CI_PROJECT_URL', env)
+    la(is.unemptyString(env.CI_BUILD_NAME), 'missing CI_BUILD_NAME', env)
+
+    const buildUrl = `${env.CI_PROJECT_URL}/builds/${env.CI_BUILD_ID}`
+    const info = {
+      // test info
+      specName: env.CI_BUILD_NAME,
+      // build info
+      buildUrl,
+      buildName: env.CI_BUILD_NAME,
+      buildId: env.CI_BUILD_ID,
+      screenshotsUrl: `${buildUrl}/artifacts/browse/cypress/screenshots/`,
+      // pipeline
+      pipelineUrl: `${env.CI_PROJECT_URL}/pipelines/${env.CI_PIPELINE_ID}`,
+      pipelineId: env.CI_PIPELINE_ID,
+      // project info
+      projectName: env.CI_PROJECT_NAME || projectName,
+      projectUrl: env.CI_PROJECT_URL
+    }
+
+    info.toString = buildToString.bind(info, info)
+    return info
+  }
+
+  function buildToString (info) {
+    la(is.object(info), 'expect info object', info)
+
+    const buildInfo = `
+==============================
+ Build information (GitLab)
+==============================
+Project name: ${info.projectName}
+Build spec name: ${info.specName}
+Build ID: ${info.buildId}
+Build URL: ${info.buildUrl}
+Screenshots URL: ${info.screenshotsUrl}
+Pipeline ID: ${info.pipelineId}
+Pipeline URL: ${info.pipelineUrl}
+Project URL: ${info.projectUrl}
+`
+    return buildInfo
+  }
+
+  return {
+    isOnGitLab,
+    collectionInformation
+  }
+}
+
+module.exports = init
+
